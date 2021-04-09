74ce6492f8517c17e39b50cde804071fc5023f03
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-09 18:42:58 +0300

    Add the main component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../header';
+import Main from '../main';
 import Footer from '../footer';
 
 import './app.css';
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <>
       <Header/>
+      <Main/>
       <Footer/>
     </>
   );
---
+++ src/components/main/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './main.css';
+
+const Main = () => {
+  return (
+    <main className="main container">
+      main
+    </main>
+  );
+}
+
+export default Main;
