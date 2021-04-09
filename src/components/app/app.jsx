d941ba184e7dc8c727afe38b1046ef9bad89c1b5
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-09 12:40:56 +0300

    Include libraries. Add the app and the nav-bar component struct

---
+++ src/components/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavBar from '../nav-bar';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/app';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    Hello World!
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
