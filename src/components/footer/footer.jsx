ea06a26091920887893dcac8ecdfc30bcef05c7e
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-09 18:20:50 +0300

    Add the footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-4">
+            TrueCoder
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-4">
+            Powered by <i>www.jdoodle.com</i>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
