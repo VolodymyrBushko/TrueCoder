92905bc39239e76f437762a39b26d010eaeb51e4
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-10 12:01:16 +0300

    Add the row component and base components of the monitor component

---
+++ src/components/row/row.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './row.css';
+
+const Row = ({left, right, sizeL = 6, sizeR = 6, defaultSize = 12}) => {
+
+  let columnL = null;
+  let columnR = null;
+  let hasAllColumns = false;
+
+  if (left && right) {
+    hasAllColumns = true;
+  }
+
+  if (left) {
+    columnL = <div className={`col-${hasAllColumns ? sizeL : defaultSize}`}>{left}</div>;
+  }
+  if (right) {
+    columnR = <div className={`col-${hasAllColumns ? sizeR : defaultSize}`}>{right}</div>;
+  }
+
+  return (
+    <div className="row">
+      {columnL}
+      {columnR}
+    </div>
+  );
+}
+
+export default Row;
