065c3601780753df9ca0e10b931ae65dece39123
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-12 13:04:59 +0300

    User register and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const jwtSecret = config.get('jwtSecret');
+
 module.exports = {
 
   async registerUser(req, res) {
@@ -6,8 +11,42 @@ module.exports = {
       const postsRef = global.db.ref('/').child('users');
       const newPostRef = postsRef.push();
 
-      const {login, email, password} = req.body;
+      let {login, email, password} = req.body;
+
+      const salt = await bcrypt.genSalt(10);
+      password = await bcrypt.hash(password, salt);
+
       newPostRef.set({login, email, password});
+      await res.status(201).json({login, email, password});
+
+    } catch (ex) {
+      console.log(ex);
+      await res.status(500).json({message: ex.message});
+    }
+  },
+
+  async loginUser(req, res) {
+    try {
+
+      const ref = global.db.ref('/').child('users');
+      const {login, password} = req.body;
+
+      ref.orderByChild("login").equalTo(login).on("child_added", snapshot => {
+
+        const {login: dblogin, email: dbemail, password: dbpassword} = snapshot.val();
+        const passwordResult = bcrypt.compareSync(password, dbpassword);
+
+        if (passwordResult) {
+          const token = jwt.sign({
+            id: snapshot.key,
+            login: dblogin,
+            email: dbemail
+          }, jwtSecret, {expiresIn: 60 * 60 * 60});
+          res.json({token: `Bearer ${token}`});
+        } else {
+          res.status(401).json({message: 'invalid login or password'});
+        }
+      });
 
     } catch (ex) {
       console.log(ex);
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,5 +3,6 @@ const userController = require("../controllers/userController.js");
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.registerUser);
+userRouter.post("/login", userController.loginUser);
 
 module.exports = userRouter;
