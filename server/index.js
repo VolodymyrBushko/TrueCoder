5b67711aaa5295ef861ebde06bd9323b86aefde7
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-12 10:19:54 +0300

    Base server struct

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-console.log('Hello from server');
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('config');
+
+const PORT = config.get('port');
+
+const app = express();
+
+app.use(require('cors')());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+async function start() {
+  try {
+    app.listen(PORT, function () {
+      console.log(`Server has been started on port: ${PORT}`);
+    });
+  } catch (ex) {
+    console.log('Server error', e.message);
+    process.exit();
+  }
+}
+
+start();
+
+process.on('SIGINT', () => {
+  console.log('Server has been finished');
+});
